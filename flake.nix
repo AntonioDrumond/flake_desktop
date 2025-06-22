22855759bcf50917b8235f51d51f4ff99435473e
Antonio Drumond <adcdstom@gmail.com>
2025-06-22 16:08:47 +0000

    finally

--- flake.nix
+++ flake.nix
@@ -4,21 +4,22 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-		nixvim.url = "github:AntonioDrumond/nixvim";
+    nixvim.url = "github:AntonioDrumond/nixvim";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: 
-	let
-		system = "x86_64-linux";
-		pkgs = nixpkgs.legacyPackages.${system};
-	in
-	{
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-			specialArgs = {inherit inputs;};
-      modules = [
-				# Import configuration.nix file
-        ./configuration.nix
-      ];
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          # Import configuration.nix file
+          ./configuration.nix
+        ];
+      };
     };
-  };
 }
