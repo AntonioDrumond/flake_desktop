bc868271075d88e9174e02c65fe8aaa4e179baf2
Antonio Drumond <adcdstom@gmail.com>
2025-06-21 23:31:56 -0300

    update flake system version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS System Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
 		nixvim.url = "github:AntonioDrumond/nixvim";
   };
