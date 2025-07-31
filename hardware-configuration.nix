6d5db33372ba4a89361ed6bec1d6484263079d82
Antonio Drumond <adcdstom@gmail.com>
2025-07-31 18:03:53 +0000

    remove failed disk

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -24,17 +24,17 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  /*
   fileSystems."/home/antonio/Disks/storage" =
-    { device = "/dev/disk/by-uuid/57ad4be2-d4aa-478b-8d2a-f4356922dff3";
+    { device = "/dev/disk/by-label/storage";
       fsType = "ext4";
     };
-  */
 
+/*
   fileSystems."/home/antonio/Disks/pirate" =
     { device = "/dev/disk/by-uuid/6756bf95-e86c-4d19-9a02-f2591766457e";
       fsType = "ext4";
     };
+*/
 
   swapDevices =
     [ { device = "/dev/disk/by-uuid/dc30a5c7-b31e-4f09-9417-ca3a44d0e491"; }
