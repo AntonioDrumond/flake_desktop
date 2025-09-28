fd060488389607838ffc1f4cb188dc674d411c7b
Antonio Drumond <adcdstom@gmail.com>
2025-09-28 17:32:56 +0000

    add flatpaks + yazi + reindent

--- configuration.nix
+++ configuration.nix
@@ -1,100 +1,106 @@
 { config, pkgs, inputs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+    imports = [
+        ./hardware-configuration.nix
+    ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  systemd.tmpfiles.rules = [
-    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
-  ];
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    systemd.tmpfiles.rules = [
+        "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+    ];
 
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
+    networking.hostName = "nixos";
+    networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time = {
-    # timeZone = "America/Sao_Paulo";
-    # timeZone = "Europe/Dublin";
-    timeZone = "Atlantic/Reykjavik";
-    hardwareClockInLocalTime = true;
-  };
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
-  };
-
-  services = {
-    xserver = {
-      enable = true;
-      wacom.enable = true;
-      xkb.layout = "us";
-      videoDrivers = [ "amdgpu" ];
+    # Set your time zone.
+    time = {
+        # timeZone = "America/Sao_Paulo";
+        # timeZone = "Europe/Dublin";
+        timeZone = "Atlantic/Reykjavik";
+        hardwareClockInLocalTime = true;
     };
-    displayManager.sddm = {
-      enable = true;
-      autoNumlock = true;
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "pt_BR.UTF-8";
+        LC_IDENTIFICATION = "pt_BR.UTF-8";
+        LC_MEASUREMENT = "pt_BR.UTF-8";
+        LC_MONETARY = "pt_BR.UTF-8";
+        LC_NAME = "pt_BR.UTF-8";
+        LC_NUMERIC = "pt_BR.UTF-8";
+        LC_PAPER = "pt_BR.UTF-8";
+        LC_TELEPHONE = "pt_BR.UTF-8";
+        LC_TIME = "pt_BR.UTF-8";
     };
-    desktopManager.plasma6.enable = true;
-    openssh = {
-      enable = true;
-      ports = [ 22 ];
-      settings = {
-        PasswordAuthentication = true;
-        AllowUsers = null;
-        UseDns = true;
-        X11Forwarding = false;
-        PermitRootLogin = "no";
-      };
+
+    services = {
+        xserver = {
+            enable = true;
+            wacom.enable = true;
+            xkb.layout = "us";
+            videoDrivers = [ "amdgpu" ];
+        };
+        displayManager.sddm = {
+            enable = true;
+            autoNumlock = true;
+        };
+        desktopManager.plasma6.enable = true;
+        openssh = {
+            enable = true;
+            ports = [ 22 ];
+            settings = {
+                PasswordAuthentication = true;
+                AllowUsers = null;
+                UseDns = true;
+                X11Forwarding = false;
+                PermitRootLogin = "no";
+            };
+        };
+        printing.enable = true;
+        flatpak = {
+            enable = true;
+            packages = [
+                { appId = "net.waterfox.waterfox"; origin = "flathub"; }
+                { appId = "com.stremio.Stremio"; origin = "flathub"; }
+            ];
+        };
     };
-  };
 
-  # Configure console keymap
-  # console.keyMap = "br-abnt2";
-  console.keyMap = "us";
+    # Configure console keymap
+    # console.keyMap = "br-abnt2";
+    console.keyMap = "us";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
 
-  # Bluetooth
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-  };
-
-  # Enable sound
-  security.rtkit.enable = true;
-  services = {
-    pulseaudio.enable = false;
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
-      wireplumber.enable = true;
+    # Bluetooth
+    hardware.bluetooth = {
+        enable = true;
+        powerOnBoot = true;
     };
-  };
 
-  # Audio fix
-  hardware.enableAllFirmware = true;
-  boot.extraModprobeConfig = ''
-    options snd-intel-dspcfg dsp_driver=1
-  '';
+    # Enable sound
+    security.rtkit.enable = true;
+    services = {
+        pulseaudio.enable = false;
+        pipewire = {
+            enable = true;
+            alsa.enable = true;
+            alsa.support32Bit = true;
+            pulse.enable = true;
+            jack.enable = true;
+            wireplumber.enable = true;
+        };
+    };
+
+    # Audio fix
+    hardware.enableAllFirmware = true;
+    boot.extraModprobeConfig = ''
+        options snd-intel-dspcfg dsp_driver=1
+    '';
 
     hardware.graphics = {
         enable = true;
@@ -103,158 +109,159 @@
         ];
     };
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  users.users.antonio = {
-    isNormalUser = true;
-    description = "Antonio Drumond";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-      "libvirtd"
-      "video"
+    nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
     ];
-    packages = with pkgs; [
-      kdePackages.kate
-    ];
-  };
 
-  programs = {
-    # Firefox
-    firefox.enable = true;
+    # Enable touchpad support (enabled default in most desktopManager).
+    # services.xserver.libinput.enable = true;
 
-    # Steam
-    steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      extraCompatPackages = with pkgs; [
-        proton-ge-bin
-      ];
+    users.users.antonio = {
+        isNormalUser = true;
+        description = "Antonio Drumond";
+        extraGroups = [
+            "networkmanager"
+            "wheel"
+            "libvirtd"
+            "video"
+        ];
+        packages = with pkgs; [
+            kdePackages.kate
+        ];
     };
 
-    # Starship
-    starship = {
-      enable = true;
-      settings = {
-        add_newline = true;
-        # "$schema" = "https://starship.rs/config-schema.json";
-        character = {
-          success_symbol = "[❯](bold green)";
-          error_symbol = "[❯](bold bright green)";
+    programs = {
+        # Firefox
+        firefox.enable = true;
+
+        # Steam
+        steam = {
+            enable = true;
+            remotePlay.openFirewall = true;
+            extraCompatPackages = with pkgs; [
+                proton-ge-bin
+            ];
         };
-        nix_shell = {
-          symbol = " ";
-          heuristic = false;
+
+        # Starship
+        starship = {
+            enable = true;
+            settings = {
+                add_newline = true;
+                # "$schema" = "https://starship.rs/config-schema.json";
+                character = {
+                    success_symbol = "[❯](bold green)";
+                    error_symbol = "[❯](bold bright green)";
+                };
+                nix_shell = {
+                    symbol = " ";
+                    heuristic = false;
+                };
+                username = {
+                    show_always = true;
+                    style_user = "green bold";
+                    style_root = "bright-red bold";
+                };
+                os.disabled = false;
+                # palette = "catppuccin_${"mocha"}";
+            };
         };
-        username = {
-          show_always = true;
-          style_user = "green bold";
-          style_root = "bright-red bold";
-        };
-        os.disabled = false;
-        # palette = "catppuccin_${"mocha"}";
-      };
+
     };
 
-  };
+    # Allow unfree packages
+    nixpkgs.config = {
+        allowUnfree = true;
+        permittedInsecurePackages = [ "ventoy-1.1.07" /*"qtwebengine-5.15.19"*/ ];
 
-  # Allow unfree packages
-  nixpkgs.config = {
-    allowUnfree = true;
-    permittedInsecurePackages = [ "ventoy-1.1.07" /*"qtwebengine-5.15.19"*/ ];
+        fonts.packages = with pkgs; [
+            noto-fonts
+            noto-fonts-cjk-sans
+            noto-fonts-emoji
+            liberation_ttf
+            fira-code
+            fira-code-symbols
+            mplus-outline-fonts.githubRelease
+            dina-font
+            proggyfonts
+            jetbrains-mono
+            nerd-fonts.jetbrains-mono
+            font-awesome
+        ];
+    };
 
-    fonts.packages = with pkgs; [
-        noto-fonts
-        noto-fonts-cjk-sans
-        noto-fonts-emoji
-        liberation_ttf
-        fira-code
-        fira-code-symbols
-        mplus-outline-fonts.githubRelease
-        dina-font
-        proggyfonts
+    # Global system packages
+    environment.systemPackages = with pkgs; [
+
+        #CLI -> Essential
+        vim
+        git
+        wezterm
+        kitty
+        fzf
+        zip
+        unzip
+        # inputs.nixvim.packages.${pkgs.system}.default # NixVim
+
+        #CLI -> Pretty and shiny :D
+        bat
+        tree
+        eza # -> better to do in programs. but couldnt yet
+        neofetch
+        superfile
+        du-dust
+        yazi
+
+        #Image stuff
+        pix
+        rawtherapee
+        krita
+        davinci-resolve
+
+        #Non raw text
+        obsidian
+        foliate
+
+        #Torrenting and stuff
+        # qbittorrent
+        # mullvad
+
+        #Nonfree entertainment
+        stremio
+        discord
+        spotify
+
+        #Virtual Machine
+        quickemu
+        spice
+
+        #Hardware/Libs
+        ventoy
+        gparted
+        fan2go
+        radeontop
+        btop
+        amdgpu_top
+        amdvlk
+        mesa
+
+        #Other
+        kdePackages.kcalc
+        cava
         jetbrains-mono
-        nerd-fonts.jetbrains-mono
-        font-awesome
+        mars-mips
+        blender-hip
     ];
-  };
 
-  # Global system packages
-  environment.systemPackages = with pkgs; [
+    environment.localBinInPath = true;
 
-    #CLI -> Essential
-    vim
-    git
-    wezterm
-    kitty
-    fzf
-    zip
-    unzip
-    # inputs.nixvim.packages.${pkgs.system}.default # NixVim
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
 
-    #CLI -> Pretty and shiny :D
-    bat
-    tree
-    eza # -> better to do in programs. but couldnt yet
-    neofetch
-    superfile
-    du-dust
-
-    #Image stuff
-    pix
-    rawtherapee
-    krita
-    davinci-resolve
-
-    #Non raw text
-    obsidian
-    foliate
-
-    #Torrenting and stuff
-    # qbittorrent
-    # mullvad
-
-    #Nonfree entertainment
-    stremio
-    discord
-    spotify
-
-    #Virtual Machine
-    quickemu
-    spice
-
-    #Hardware/Libs
-    ventoy
-    gparted
-    fan2go
-    radeontop
-    btop
-    amdgpu_top
-    amdvlk
-    mesa
-
-    #Other
-    kdePackages.kcalc
-    cava
-    jetbrains-mono
-    mars-mips
-    blender-hip
-  ];
-
-  environment.localBinInPath = true;
-
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # DO NOT CHANGE
-  system.stateVersion = "24.11"; # Did you read the comment?
+    # DO NOT CHANGE
+    system.stateVersion = "24.11"; # Did you read the comment?
 
 }
