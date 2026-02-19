f7a77350c332602ca2cf5c0408fc87f2b484f16a
Antonio Drumond <adcdstom@gmail.com>
2026-02-19 13:05:53 +0000

    fix stuff

--- syncing_scripts/bring_here.sh
+++ syncing_scripts/bring_here.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # .config
-cd /home/antonio/repos/flake_laptop/dot_config
+cd /home/antonio/repos/flake_desktop/dot_config
 cp -r /home/antonio/.config/hypr .
 cp -r /home/antonio/.config/kitty .
 cp -r /home/antonio/.config/waybar .
 cp -r /home/antonio/.config/fish .
 
 # ~
-cd /home/antonio/repos/flake_laptop/dots
+cd /home/antonio/repos/flake_desktop/dots
 cp /home/antonio/.bashrc .
 cp /home/antonio/.gitconfig .
 cp /home/antonio/.vimrc .
--- syncing_scripts/update_local.sh
+++ syncing_scripts/update_local.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # .config
-cd /home/antonio/repos/flake_laptop/dot_config
+cd /home/antonio/repos/flake_desktop/dot_config
 cp -r ./hypr/ /home/antonio/.config/
 cp -r ./kitty/ /home/antonio/.config/
 cp -r ./waybar/ /home/antonio/.config/
 cp -r ./fish/ /home/antonio/.config/
 
 # ~
-cd /home/antonio/repos/flake_laptop/dots
+cd /home/antonio/repos/flake_desktop/dots
 cp ./.* /home/antonio
